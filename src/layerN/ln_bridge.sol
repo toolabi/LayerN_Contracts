063900b5e61863fbf9d456823df8bec243bbab3e
Pardis <prds.toolabi@gmail.com>
2025-02-27 11:45:04 +0100

    add withdraw

--- src/layerN/ln_bridge.sol
+++ src/layerN/ln_bridge.sol
@@ -5,6 +5,34 @@ contract LnBridge {
 // depoloy new tokens id they dont exis
 // update balances
 
+    enum ActionType {
+        Deposit,
+        Withdraw
+    }
+
+    struct Action {
+        ActionType action;
+        address token;
+        uint256 amount;
+    }
+
+    event LogAction(Action);
+
+    mapping(address => mapping(address => uint256)) public balances;
+    mapping(address => address) public tokens;
+
+
+    function deposit(Action memory _deposit) public {
+        require(_deposit.token != address(0), "Invalid input.");
+        address addr = tokens[_deposit.token];
+        if (addr == address(0)){
+
+        }else{
+
+        }
+
+    }
+
 
     function _getLnAddress(address _token) internal returns(address){
         bytes32 identifier = keccak256(abi.encode(address(_token)));
@@ -12,4 +40,7 @@ contract LnBridge {
         bytes32 preimage =keccak256(bytes.concat(identifier, salt));
         return address(uint160(uint256(preimage)));
     }
+
+
+    
 }
