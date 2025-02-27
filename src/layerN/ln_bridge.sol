1dd01a5bd5cef15ba3642d241301885723fb5b43
Pardis <prds.toolabi@gmail.com>
2025-02-27 12:11:49 +0100

    add deposit

--- src/layerN/ln_bridge.sol
+++ src/layerN/ln_bridge.sol
@@ -1,9 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
+
+import "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+
+
 contract LnBridge {
 // depoloy new tokens id they dont exis
 // update balances
+
+    bytes public erc20BytecodeHash;
 
     enum ActionType {
         Deposit,
@@ -17,30 +24,53 @@ contract LnBridge {
     }
 
     event LogAction(Action);
+    event Deployed(address, uint256);
 
     mapping(address => mapping(address => uint256)) public balances;
     mapping(address => address) public tokens;
 
+    constructor(bytes memory _erc20BytecodeHash){
+        erc20BytecodeHash =_erc20BytecodeHash;
+    }
 
-    function deposit(Action memory _deposit) public {
+
+    function proccessDeposit(Action memory _deposit) public {
         require(_deposit.token != address(0), "Invalid input.");
         address addr = tokens[_deposit.token];
         if (addr == address(0)){
-
-        }else{
-
+            addr = _deploy(erc20BytecodeHash, _getsalt(_deposit.token));
         }
+        IERC20(_deposit.token)._mint(msg.sender, _deposit.amount);
+
 
     }
 
 
-    function _getLnAddress(address _token) internal returns(address){
+    function _getsalt(address _token) internal returns(bytes32){
         bytes32 identifier = keccak256(abi.encode(address(_token)));
         bytes32 salt = bytes32(uint256(uint160(msg.sender)));
-        bytes32 preimage =keccak256(bytes.concat(identifier, salt));
-        return address(uint160(uint256(preimage)));
+        return keccak256(bytes.concat(identifier, salt));
+    }
+
+    function _deploy(bytes memory bytecode, bytes32 _salt) internal returns(address) {
+        address addr;
+
+        assembly {
+            addr :=
+                create2(
+                    0, 
+                    add(bytecode, 0x20),
+                    mload(bytecode),
+                    _salt
+                )
+
+            if iszero(extcodesize(addr)) { revert(0, 0) }
+        }
+
+        emit Deployed(addr, _salt);
+        return addr;
     }
 
 
-    
+
 }
