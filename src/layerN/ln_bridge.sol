34305034fb830e7d92ca88bf217a6a53ab6eab29
Pardis <prds.toolabi@gmail.com>
2025-02-27 11:11:15 +0100

    add alyer n

--- src/layerN/ln_bridge.sol
+++ src/layerN/ln_bridge.sol
@@ -1,11 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-
-contract LnBridge{
-
-    // depoloy new tokens id they dont exis
-    // update balances
+contract LnBridge {
+// depoloy new tokens id they dont exis
+// update balances
 }
-
-
